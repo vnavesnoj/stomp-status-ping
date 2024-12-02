f762a5ebebeac128b805e7d1ecfdb2b8fcc6a584
vnavesnoj <vnavesnoj@gmail.com>
2024-12-02 21:34:42 +0200

    Add RedisSessionConfiguration

---
+++ src/main/java/vnavesnoj/stomp_status_ping/config/RedisSessionConfiguration.java
@@ -0,0 +1,11 @@
+package vnavesnoj.stomp_status_ping.config;
+
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisIndexedHttpSession;
+
+/**
+ * @author vnavesnoj
+ * @mail vnavesnoj@gmail.com
+ */
+@EnableRedisIndexedHttpSession
+public class RedisSessionConfiguration {
+}
