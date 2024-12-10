088d3a7353868cb3fac4d3176b678465b60c0f15
vnavesnoj <vnavesnoj@gmail.com>
2024-12-10 22:44:55 +0200

    Add StompConnectInterceptor

---
+++ src/main/java/vnavesnoj/stomp_status_ping/websocket/StompConnectInterceptor.java
@@ -0,0 +1,32 @@
+/*
+ * @author vnavesnoj
+ * @mail vnavesnoj@gmail.com
+ */
+
+package vnavesnoj.stomp_status_ping.websocket;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageType;
+import org.springframework.messaging.support.ChannelInterceptor;
+
+/**
+ * @author vnavesnoj
+ * @mail vnavesnoj@gmail.com
+ */
+@RequiredArgsConstructor
+public abstract class StompConnectInterceptor implements ChannelInterceptor {
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        final var messageType = SimpMessageHeaderAccessor.getMessageType(message.getHeaders());
+        if (messageType == SimpMessageType.CONNECT) {
+            doOnConnect(message, channel);
+        }
+        return message;
+    }
+
+    protected abstract void doOnConnect(Message<?> message, MessageChannel channel);
+}
