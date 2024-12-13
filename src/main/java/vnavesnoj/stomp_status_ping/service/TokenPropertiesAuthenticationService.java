460ba7f8e81c6d3b73d04be3742bd05f146de057
vnavesnoj <vnavesnoj@gmail.com>
2024-12-13 22:57:25 +0200

    Add TokenPropertiesAuthenticationService

---
+++ src/main/java/vnavesnoj/stomp_status_ping/service/TokenPropertiesAuthenticationService.java
@@ -0,0 +1,54 @@
+/*
+ * @author vnavesnoj
+ * @mail vnavesnoj@gmail.com
+ */
+
+package vnavesnoj.stomp_status_ping.service;
+
+import lombok.SneakyThrows;
+import org.springframework.core.io.InputStreamSource;
+import org.springframework.core.io.ResourceLoader;
+import vnavesnoj.stomp_status_ping.dto.AuthenticationResult;
+
+import java.io.IOException;
+import java.util.Optional;
+import java.util.Properties;
+import java.util.function.Predicate;
+
+/**
+ * @author vnavesnoj
+ * @mail vnavesnoj@gmail.com
+ */
+public class TokenPropertiesAuthenticationService implements TokenAuthenticationService {
+
+    private final Properties tokens;
+
+    @SneakyThrows
+    public TokenPropertiesAuthenticationService(String tokenPropertiesPath,
+                                                ResourceLoader resourceLoader) {
+        this.tokens = Optional.of(tokenPropertiesPath)
+                .map(resourceLoader::getResource)
+                .map(InputStreamSource::getInputStream)
+                .map(item -> {
+                            final var properties = new Properties();
+                            try {
+                                properties.load(item);
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                            return properties;
+                        }
+                )
+                .filter(Predicate.not(Properties::isEmpty))
+                .orElseThrow(() -> new IllegalArgumentException(tokenPropertiesPath + " is empty"));
+    }
+
+    @SneakyThrows
+    @Override
+    public AuthenticationResult getByToken(String token) {
+        return Optional.of(tokens)
+                .map(item -> item.getProperty(token))
+                .map(item -> new AuthenticationResult(true, item))
+                .orElseGet(() -> new AuthenticationResult(false, null));
+    }
+}
