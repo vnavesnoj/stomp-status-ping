59df137ad03a63ac5dd2283b007bea4f87fe514b
vnavesnoj <vnavesnoj@gmail.com>
2024-12-30 23:36:36 +0200

    Change expiredSessions map type

--- src/main/java/vnavesnoj/stomp_status_ping/websocket/interceptor/WsSessionRelevanceMatcherInterceptor.java
+++ src/main/java/vnavesnoj/stomp_status_ping/websocket/interceptor/WsSessionRelevanceMatcherInterceptor.java
@@ -1,5 +1,9 @@
 package vnavesnoj.stomp_status_ping.websocket.interceptor;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
@@ -11,9 +15,8 @@ import org.springframework.stereotype.Component;
 import vnavesnoj.stomp_status_ping.dto.SessionExpiredEvent;
 
 import java.time.Instant;
-import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 import static org.springframework.messaging.simp.stomp.DefaultStompSession.EMPTY_PAYLOAD;
 import static org.springframework.messaging.support.MessageBuilder.createMessage;
@@ -22,10 +25,14 @@ import static org.springframework.messaging.support.MessageBuilder.createMessage
  * @author vnavesnoj
  * @mail vnavesnoj@gmail.com
  */
+@RequiredArgsConstructor
 @Component
 public class WsSessionRelevanceMatcherInterceptor implements ChannelInterceptor, ApplicationListener<SessionExpiredEvent> {
 
-    private final Map<String, ExpiredSession> expiredSessions = new ConcurrentHashMap<>();
+    private final Cache<String, ExpiredSession> expiredSessions = CacheBuilder.newBuilder()
+            .concurrencyLevel(1)
+            .expireAfterWrite(10L, TimeUnit.MINUTES)
+            .build();
     public static final String ERROR_MESSAGE = "Session expired";
 
     @Override
@@ -33,11 +40,11 @@ public class WsSessionRelevanceMatcherInterceptor implements ChannelInterceptor,
         final var errorMessage = Optional.of(message)
                 .map(Message::getHeaders)
                 .map(SimpMessageHeaderAccessor::getSessionId)
-                .filter(expiredSessions::containsKey)
+                .map(expiredSessions::getIfPresent)
                 .map(item -> {
-                    expiredSessions.remove(item);
+                    expiredSessions.invalidate(item.getSessionId());
                     final var accessor = StompHeaderAccessor.create(StompCommand.ERROR);
-                    accessor.setSessionId(item);
+                    accessor.setSessionId(item.getSessionId());
                     accessor.setMessage(ERROR_MESSAGE);
                     return createMessage(EMPTY_PAYLOAD, accessor.getMessageHeaders());
                 });
@@ -57,7 +64,10 @@ public class WsSessionRelevanceMatcherInterceptor implements ChannelInterceptor,
         expiredSessions.put(event.getSessionId(), session);
     }
 
+    @Value
+    private static class ExpiredSession {
 
-    private record ExpiredSession(String sessionId, Instant expiredAd) {
+        String sessionId;
+        Instant expiredAd;
     }
 }
