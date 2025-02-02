15f4141bd83651c7d1c05a0d05a27752c495353e
vnavesnoj <vnavesnoj@gmail.com>
2025-02-02 19:23:54 +0200

    Add CookieRequestConverter

---
+++ src/main/java/vnavesnoj/stomp_status_ping/security/CookieRequestConverter.java
@@ -0,0 +1,34 @@
+package vnavesnoj.stomp_status_ping.security;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.ott.OneTimeTokenAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationConverter;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+/**
+ * @author vnavesnoj
+ * @mail vnavesnoj@gmail.com
+ */
+@RequiredArgsConstructor
+public class CookieRequestConverter implements AuthenticationConverter {
+
+    private final String cookieName;
+
+    @Override
+    public Authentication convert(HttpServletRequest request) {
+        return Optional.of(request)
+                .map(HttpServletRequest::getCookies)
+                .flatMap(cookies -> Arrays.stream(cookies)
+                        .map(Cookie::getName)
+                        .filter(cookieName::equals)
+                        .findFirst()
+                )
+                .map(OneTimeTokenAuthenticationToken::unauthenticated)
+                .orElse(null);
+    }
+}
